29c5f584cd759de2f9de80d8e9d4865c4c04c3a7
Octaze0406 <142670306+Octaze0406@users.noreply.github.com>
2023-11-17 18:56:09 +0530

    Create crackme.py

---
+++ Reverse/crackme.py
@@ -0,0 +1,7 @@
+picoCTF{1|\/|_4_p34|\|ut_ef5b69a3}  
+First file crackme.py showed it was a ascii file with python code.  
+Open the file. cat crackme.py
+It showed that, a buch of letters was stored inside a variable named *alphabet* and bezos secret was the word that we need to decrpyt using the rot 47 method.  
+explained in NOTES in Obscidian.
+  
+
